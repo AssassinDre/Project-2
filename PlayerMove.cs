cf1d2b363f1b77c8c0ec1a35a1e352c13a1e4ef4
Alexander Sacharske <sacha101@mail.chapman.edu>
2015-04-19 14:22:38 -0700

    Basic Player Movement
    Super simple, based on Audrey's 2D movement script
    
    **Currently lacks code for animations

---
+++ PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	//********************ANIMATION CODE TO BE ADDED********************
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//right movement
+		if (Input.GetKey ("right")) 
+		{
+			rigidbody2D.AddForce(Vector2.right* speed);
+		}
+		//left movement
+		if (Input.GetKey("left"))
+		{
+			rigidbody2D.AddForce(-Vector2.right* speed);
+		}
+}
+}
