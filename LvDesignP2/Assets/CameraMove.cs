91d79c87a55cf071c49d07f2356977cb58d28e71
AssassinDre <will104@mail.chapman.edu>
2015-04-27 23:34:21 -0700

    New Version
    I remade the project with it initially in 2D so it would take the
    sprites. Right now it has the bar bg, player movement and animation, and
    sprites for the people. The talk sprites for the two people are also in
    there for when dialogue is figured out.

---
+++ LvDesignP2/Assets/CameraMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public GameObject player;
+	public float x;
+	public float y;
+	
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3(x, y, -20f);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (player.transform.position.x < 5.15f && player.transform.position.x > -4.965){
+			transform.position = player.transform.position + offset;
+		}
+	}
+}
---
+++ LvDesignP2/Assets/PlayerMove2.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove2 : MonoBehaviour {
+	
+	bool isPaused = false;
+	private Vector2 pos;
+	public float speed = 1f;
+	DoorMove doorMove;
+	public GameObject Door1A, Door1B, Door2A, Door2B;
+	bool moving = false;
+	bool door = false;
+	bool displayInven = false;
+	bool collected = false;
+	public Texture2D dummyInventory;
+	private Animator animator;
+	public bool left = false;
+	public bool right = true;
+	
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+		doorMove = new DoorMove ();
+		animator = this.GetComponent<Animator> ();
+	}
+
+	void OnGUI()
+	{
+		GUIStyle myLabelStyle = new GUIStyle(GUI.skin.label);
+		myLabelStyle.normal.textColor = Color.red;
+		myLabelStyle.fontSize = 50;
+
+		if (displayInven) {
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Dummy Inventory Screen");
+			if (collected) {
+				GUI.DrawTexture (new Rect(100, 100, 100, 100), dummyInventory);
+			}
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkInput ();
+		Debug.Log("Moving: " + moving);
+		Debug.Log("Door: " + door);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		moving = true;
+		if (other.tag == "Collectable") {
+			Destroy (other.gameObject);
+			collected = true;
+			Debug.Log (collected);
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Door1A" && door == true) {
+			transform.position = Door1B.transform.position;
+		}
+
+		if (other.gameObject.tag == "Door1B" && door == true) {
+			transform.position = Door1A.transform.position;
+		}
+
+		if (other.gameObject.tag == "Door2A" && door == true) {
+			transform.position = Door2B.transform.position;
+		}
+		
+		if (other.gameObject.tag == "Door2B" && door == true) {
+			transform.position = Door2A.transform.position;
+		}
+	}
+
+	void OnTriggerExit(){
+		moving = false ;
+		door = false;
+	}
+
+	
+	
+	private void checkInput(){
+		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.I)) {
+			if (isPaused == false) isPaused = true;
+			else if (isPaused == true) isPaused = false;
+			if (displayInven == false) displayInven = true;
+			else if (displayInven == true) displayInven = false;
+		}
+
+		if (!isPaused) {
+			if (Input.GetKey (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow)) {
+				Debug.Log ("Acitavated");
+				transform.position += Vector3.right * speed * Time.deltaTime;
+				animator.SetBool("HoldRight",true);
+				animator.SetBool("HoldLeft",false);
+				right = true;
+				left = false;
+			}
+			// For left, we have to subtract the direction
+			else if (Input.GetKey (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow)) {
+				transform.position -= Vector3.right * speed * Time.deltaTime;
+				animator.SetBool("HoldLeft",true);
+				animator.SetBool("HoldRight",false);
+				left = true;
+				right = false;
+			}
+			else if (Input.GetKey (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) {
+				if (moving == true)
+				{
+					door = true;
+					Debug.Log ("Worked");
+				}
+
+			}
+			
+			// Same as for the left, subtraction for down
+			else if (Input.GetKey (KeyCode.S) || Input.GetKeyDown (KeyCode.DownArrow)) {
+				
+			}
+			else if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow)) {
+				animator.SetBool("HoldRight",false);
+			}
+			else if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow)) {
+				animator.SetBool("HoldLeft",false);
+			}
+			if (left == true){
+				animator.SetBool("Left",true);
+				animator.SetBool("Right",false);
+			}
+			if (right == true){
+				animator.SetBool("Left",false);
+				animator.SetBool("Right",true);
+			}
+		}
+	}
+}
