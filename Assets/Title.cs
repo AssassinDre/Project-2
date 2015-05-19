fa3092b5566f796b4cb240052ebd2f9a5ced5cba
AssassinDre <will104@mail.chapman.edu>
2015-05-18 23:50:18 -0700

    Title Screen

---
+++ Assets/Title.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+
+		if (GUI.Button(new Rect(Screen.width/1.55f, Screen.height/1.5f, 100, 40), "Start"))
+		{
+			Application.LoadLevel("0.Black");
+		}
+		if (GUI.Button(new Rect(Screen.width/1.55f, Screen.height/1.3f, 100, 40), "Quit"))
+		{
+			Application.Quit ();
+		}
+		
+		
+	}
+}
