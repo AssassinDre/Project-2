2dba340723d278a5867e1cd62b6ce1106832586b
AssassinDre <will104@mail.chapman.edu>
2015-05-19 11:58:27 -0700

    End Screens

---
+++ Assets/EndCard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndCard : MonoBehaviour {
+	public bool show = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+		if (show == true){
+		
+			if (GUI.Button(new Rect(Screen.width/4f, Screen.height/1.5f, 100, 40), "Menu"))
+			{
+				Application.LoadLevel("Title");
+			}
+			if (GUI.Button(new Rect(Screen.width/1.6f, Screen.height/1.5f, 100, 40), "Quit"))
+			{
+				Application.Quit ();
+			}
+		}
+	}
+	void Show(){
+		show = true;
+	}
+}
